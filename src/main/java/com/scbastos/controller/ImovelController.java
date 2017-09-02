c809755c00b19d17cd6c396e779e6fb31bfe932c
brsferreira <brunoferreira.tecnologia@gmail.com>
2017-09-01 21:27:03 -0300

    21:26 01/09/2017

--- src/main/java/com/scbastos/controller/ImovelController.java
+++ src/main/java/com/scbastos/controller/ImovelController.java
@@ -1,15 +1,49 @@
 package com.scbastos.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.scbastos.model.Classificacao;
+import com.scbastos.model.Conservacao;
+import com.scbastos.model.Destinacao;
+import com.scbastos.model.Imovel;
+import com.scbastos.model.Situacao;
+import com.scbastos.model.TipoImovel;
 
 @Controller
 public class ImovelController {
 	
 	@RequestMapping("/imovel/novo")
-	public String novo(){
-		
-		return "imovel/CadastroImovel";
+	public ModelAndView novo(Imovel imovel){
+		ModelAndView mv = new ModelAndView("imovel/CadastroImovel");
+		mv.addObject("tipoImoveis", TipoImovel.values());
+		mv.addObject("destinacoes", Destinacao.values());
+		mv.addObject("conservacao", Conservacao.values());
+		mv.addObject("classificacoes", Classificacao.values());
+		mv.addObject("situacoes", Situacao.values());
+		return mv;
 
 	}
-}
+	
+	@RequestMapping(value = "/imovel/novo", method = RequestMethod.POST)
+	public ModelAndView cadastrar(@Valid Imovel imovel, BindingResult result, Model model, RedirectAttributes atributes){
+		
+		if(result.hasErrors()){
+			return novo(imovel);
+		}
+		
+		// Salvar no banco de dados
+		atributes.addFlashAttribute("mensagem", "Imovel cadastrado com sucesso");
+		System.out.println(">>>> cadastrar:" + imovel.getCodigocs());
+		return new ModelAndView("redirect:/imovel/novo");
+		
+	}
+	
+}//END CLASS
--- src/main/java/com/scbastos/controller/ProprietarioController.java
+++ src/main/java/com/scbastos/controller/ProprietarioController.java
@@ -2,57 +2,37 @@ package com.scbastos.controller;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.scbastos.model.Proprietario;
-import com.scbastos.repository.Proprietarios;
 
 @Controller
 public class ProprietarioController {
 	
-	
-	private static final Logger logger = LoggerFactory.getLogger(ProprietarioController.class);
-	
-	//BUSCAR DO REPOSITORIO
-	@Autowired
-	private Proprietarios proprietarios;
-			
 			
 	@RequestMapping("/proprietario/novo")
-	public String novo(Proprietario proprietario) {
-		
-		if(logger.isDebugEnabled() ){
-			logger.debug("O objeto: " + proprietario);
-		}
-		
-		proprietarios.findAll();//teste e apagar
-		return "proprietario/CadastroProprietario";
+	public ModelAndView novo(Proprietario proprietario) {
+		ModelAndView mv = new ModelAndView("proprietario/CadastroProprietario");
+		return mv;
 	}
 	
 	@RequestMapping(value = "/proprietario/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Proprietario proprietario, BindingResult result, Model model, RedirectAttributes atributes){
+	public ModelAndView cadastrar(@Valid Proprietario proprietario, BindingResult result, Model model, RedirectAttributes atributes){
 		
 		if(result.hasErrors()){
 			return novo(proprietario);
 		}
 		
-		atributes.addFlashAttribute("mensagem", "Cerveja cadastrada com sucesso");
+		// Salvar no banco de dados
+		atributes.addFlashAttribute("mensagem", "Proprietário cadastrado com sucesso");
 		System.out.println(">>>> cadastrar:" + proprietario.getNome());
-		return "redirect:/proprietario/novo";
+		return new ModelAndView("redirect:/proprietario/novo");
 		
 	}
 	
-	@RequestMapping("/proprietario/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
-
 }//END CLASS
